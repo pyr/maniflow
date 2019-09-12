266c16bc94e70ecf888243d02a3a18f10eee072d
Pierre-Yves Ritschard <pyr@spootnik.org>
2019-09-12 13:50:47 +0200

    Version 0.1.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject spootnik/maniflow "0.1.0"
+(defproject spootnik/maniflow "0.1.1-SNAPSHOT"
   :description "Additional utilies on top of manifold"
   :url "https://github.com/pyr/maniflow"
   :license {:name "MIT/ISC License"
